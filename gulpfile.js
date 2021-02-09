ab32bf8573ed4b6993895a07aa71e6f2823b15f3
Alexis Munoz <alexis.munoz@wizeline.com>
2021-02-09 16:54:51 -0600

    Finish base project with live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,50 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
 let typescript = require('gulp-typescript');
+let webserver = require('gulp-webserver');
+
+const BUILD_PATH = 'dist';
 
 gulp.task('html', function () {
     return gulp.src('src/index.html')
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(BUILD_PATH));
 });
 
+gulp.task('watch:html', gulp.series('html', function(done) {
+    gulp.watch('src/**/*.html', gulp.series('html'));
+    done();
+}));
 
 gulp.task('styles', function () {
     return gulp.src('src/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('dist/assets/styles'));
+    .pipe(gulp.dest(BUILD_PATH + '/assets/styles'));
 });
+
+gulp.task('watch:styles', gulp.series('styles', function(done) {
+    gulp.watch('src/**/*.scss', gulp.series('styles'));
+    done();
+}));
 
 gulp.task('scripts', function () {
     let tsConfig = typescript.createProject('tsconfig.json');
     return tsConfig.src()
     .pipe(typescript())
-    .pipe(gulp.dest('dist/assets/scripts'));
+    .pipe(gulp.dest(BUILD_PATH + '/assets/scripts'));
 });
 
-gulp.task('default', gulp.parallel(['styles', 'scripts', 'html']));
+gulp.task('watch:scripts', gulp.series('scripts', function(done) {
+    gulp.watch('src/**/*.ts', gulp.series('scripts'));
+    done();
+}));
+
+gulp.task('serve', function() {
+    return gulp.src(BUILD_PATH).pipe(
+        webserver({
+            open:true,
+            livereload: true
+        })
+    );
+})
+
+gulp.task('default', gulp.parallel(['watch:styles',  'watch:html', 'watch:scripts', 'serve']));
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,1 @@
-console.log("hello");
+console.log("ts file");
