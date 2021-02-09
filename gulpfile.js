e38eee79ef051d164e5651d30758fd118b24686a
Alexis Munoz <alexis.munoz@wizeline.com>
2021-02-08 18:21:04 -0600

    Complete gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let typescript = require('gulp-typescript');
+
+gulp.task('html', function () {
+    return gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('styles', function () {
+    return gulp.src('src/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/assets/styles'));
+});
+
+gulp.task('scripts', function () {
+    let tsConfig = typescript.createProject('tsconfig.json');
+    return tsConfig.src()
+    .pipe(typescript())
+    .pipe(gulp.dest('dist/assets/scripts'));
+});
+
+gulp.task('default', gulp.parallel(['styles', 'scripts', 'html']));
