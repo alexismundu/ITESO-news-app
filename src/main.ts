ebad303967753b3fa40cc9c6e2b02c41891ac1c0
Alexis Munoz <alexis.munoz@wizeline.com>
2021-02-10 00:53:35 -0600

    Extend display news to 20

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 declare let Handlebars: any;
 
+const NEWS_PER_ROW = 4;
+
 let searchBar = (<HTMLInputElement>document.getElementsByClassName('search-bar')[0])
+
+let apiKey = 'e5980334e1834407b04fb0813a77ce08';
 
 let url = 'http://newsapi.org/v2/everything?' +
     'q=Microsoft&' +
     'sortBy=popularity&' +
-    'apiKey=e5980334e1834407b04fb0813a77ce08';
+    `apiKey=${apiKey}`;
 
 let req = new Request(url);
 
@@ -18,31 +22,54 @@ async function fetchNews() {
     return news;
 }
 
-fetchNews().then(fetched_news => {
-    let raw_news = fetched_news['articles'].slice(0, 4);
+function displayNews() {
+    fetchNews().then(fetched_news => {
+        console.log(fetched_news['articles'].length)
+        let raw_news = fetched_news['articles'].length <= 20 ? fetched_news['articles']: fetched_news['articles'].slice(0, 20);
 
-    let news = raw_news.map(e => {
-        return {
-            urlToImage: e['urlToImage'],
-            title: e['title'],
-            description: e['description'],
-            url: e['url'],
+        let news = raw_news.map(e => {
+            return {
+                urlToImage: e['urlToImage'],
+                title: e['title'],
+                description: e['description'],
+                url: e['url'],
+            }
+        });
+
+        let rows = [];
+
+        for (let i = 0, j = news.length; i < j; i += NEWS_PER_ROW) {
+            rows.push(
+                {
+                    news: news.slice(i, i + NEWS_PER_ROW)
+                }
+            );
         }
-    })
-    console.log(news);
-    document.getElementById('news-container').innerHTML = template({
-        news: news
+
+        console.log(news);
+        document.getElementById('news-container').innerHTML = template({
+            rows: rows
+        });
     });
-});
-
-
-function search(){
-    console.log(`Searching... ${searchBar.value}`);
 }
 
-searchBar.addEventListener("keyup", function(event) {
+function search() {
+    url = 'http://newsapi.org/v2/everything?'
+        + `q=${searchBar.value}`
+        + '&sortBy=popularity'
+        + `&apiKey=${apiKey}`;
+    console.log(`Searching... ${searchBar.value}`);
+    req = new Request(url);
+
+    displayNews();
+
+}
+
+searchBar.addEventListener("keyup", function (event) {
     if (event.key === 'Enter') {
-      event.preventDefault();
-      search();
+        event.preventDefault();
+        search();
     }
-  });
+});
+
+displayNews();
