3e7c8a0d8eb87e3dc346484d489b8a8b9c078e30
Alexis Munoz <alexis.munoz@wizeline.com>
2021-03-05 19:44:31 -0600

    Move style for the register view to styles

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.urlencoded());
 app.use(bodyParser.json());
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use('/', express.static(path.join(__dirname, "public")));
 
 routes(app);
 
